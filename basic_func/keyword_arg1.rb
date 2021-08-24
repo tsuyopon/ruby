64b8c491c4fd8070373303d9ab5927c848ecc84f
Tsuyoshi Ooishi <tsuyoshiooishi@yahoo.co.jp>
2021-08-25 08:42:25 +0900

    modify keyword_arg1

--- basic_func/keyword_arg1.rb
+++ basic_func/keyword_arg1.rb
@@ -2,10 +2,18 @@
 # キーワード引数のサンプルプログラム
 # 
 
+# キーワード引数を使用する
 def func(name:, age:)
+  "name:#{name}, age:#{age}"
+end
+
+# ageが指定されない場合にはデフォルト値を使用する
+def func2(name:, age: 10)
   "name:#{name}, age:#{age}"
 end
 
 # キーワード引数の場合には引数が入れ替わっても問題がない
 p func(name: "azarashi", age: 3)
 p func(age: 5, name: "goma")
+
+p func2(name: "baikaru")
